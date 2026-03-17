ecc2e71463d99af75c4f44da8ee159afe1973b2f
abdulguna216-pixel <abdulguna216@gmail.com>
2026-03-17 18:09:44 +0300

    Update j.js

--- j.js
+++ j.js
@@ -1,13 +1,3 @@
-// Navbar scroll effect
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 100) {
-        navbar.classList.add('scrolled');
-    } else {
-        navbar.classList.remove('scrolled');
-    }
-});
-
 // Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
